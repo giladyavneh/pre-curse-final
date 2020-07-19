a859afce87927247a1e0698994ef66af0c8f4934
giladyavneh <giladov@gmail.com>
2020-07-19 23:31:52 +0300

    added a slider miniMenu for each item

--- src/main.js
+++ src/main.js
@@ -58,7 +58,21 @@ function addItem(){
         let more=document.createElement("div")
         more.className="more"
         more.innerText=":"
+        more.addEventListener("click",showMore)
+        more.style.cursor="pointer"
         container.appendChild(more)
+        //creatin the menu element
+        let menu=document.createElement("div")
+        menu.className="menu"
+        let del=document.createElement("button")
+        del.className="delete"
+        del.innerText="DELETE"
+        let edit=document.createElement("button")
+        edit.className="edit"
+        edit.innerText="EDIT"
+        menu.appendChild(edit)
+        menu.appendChild(del)
+        container.appendChild(menu)
         //appending it all to the actual page, by the priority
         target.appendChild(container)
         target.appendChild(document.createElement("hr"))
@@ -81,7 +95,9 @@ function turnDateToNumber(stamp){
     }
     return result
 }
-
+//function for creating the list headers, because when the list is modified
+//it's more simple the erase it all and reconstuct it
+//the same header needed to be generated each time
 function headerCreator(){
     let target=document.getElementById("view")
     let header=document.createElement("div")
@@ -99,16 +115,36 @@ function sortByPriority(){
     const target=document.getElementById("view")
     let kids=target.children
     let repo=[]
+    //creating an array with all list objects
     for (let i=0;i<kids.length;i++){
         if (kids[i].querySelector(".todoPriority")!=null){
             repo.push(kids[i]) 
         }   
     }
+    //sorting the array by the value inside "todoPriority" div
     repo.sort(function(a, b){return b.querySelector(".todoPriority").innerText-a.querySelector(".todoPriority").innerText})
+    //empty the list and recostuct it with the new sorted list
     target.innerHTML=""
     headerCreator()
     repo.forEach(function(elem){
         target.appendChild(elem)
         target.appendChild(document.createElement("hr"))
     })
+}
+function showMore(e){
+    let button=e.target
+    let item=button.parentElement
+    let menu=item.querySelector(".menu")
+    if(button.getAttribute("open")=="true"){
+        button.removeAttribute("open")
+        menu.style.maxWidth="0px"
+        menu.style.transform="translateX(0px)"
+        item.style.transform="translateX(0px)"
+    }
+    else{
+        button.setAttribute("open","true")
+        menu.style.maxWidth="100px"
+        menu.style.transform="translateX(100px)"
+        item.style.transform="translateX(-100px)"
+    }
 }
